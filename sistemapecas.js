9032a2ffa8a26afadbf55fbdd145a592ceb8c85e
ferdinan <ferdinandjr20202@gmail.com>
2022-09-24 08:44:38 -0300

    adicionado

---
+++ sistemapecas.js
@@ -0,0 +1,24 @@
+var listaPecas = ["motor", "freio", "volante",]
+
+let pesoPeca = 110
+
+if (pesoPeca > 100) {console.log ("cadastro permitido")}
+else {console.log ("nao cadastre: peso não alcançado")}
+
+console.log ("fim do processamento")
+
+let qtdePeca = 12
+
+if ( qtdePeca > 10) {console.log ("cadastro não permitido: limite da caixa ultrapassado")}
+else {console.log (" cadastro permitido")}
+
+console.log ("fim do processamento")
+
+let nomePecas = listaPecas[2]
+
+if ( nomePecas.length < 3 ) {console.log ("cadastro não permitido: nome inválido")}
+else {console.log (" cadastro permitido")}
+
+console.log ("fim do processamento")
+
+
